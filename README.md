a3a10dd2d43b0968409799d5d9c580a2ce70a457
Robert Agthe <robert@scriptshit.de>
2020-06-22 19:30:57 +0200

    working lookup rest

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # discord lookup service
 
-ask discord for data. atm only usernames
+ask discord for data. atm only usernames. /?user=discorduserID and you get a profile back
--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const DClient = new Discord.Client()
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-    client.user.fetch('112328864650231808').then(() => {
-      res.send()
+    if (req.query.user) {
+      DClient.login(process.env.API_DISCORD_TOKEN).then((token) => {
+        const DUser = new Discord.User(DClient, { id: req.query.user })
+        DUser.fetch()
+          .then((data) => {
+            res.json(data)
+            res.end()
+          }).catch((err) => {
+            res.json(err)
+            res.end()
+          })
+      }).catch((err) => {
+        res.json(err)
+        res.end()
+      })
+    } else {
       res.end()
-    }).catch((err) => res.end(err))
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
