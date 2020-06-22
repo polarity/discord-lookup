9c9ec347c1fe2227512386f15a85f762fbf32868
Robert Agthe <robert@scriptshit.de>
2020-06-22 18:40:54 +0200

    unnessecary code removed, added discord bot

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => {
+    res.send(client.fetchUser('112328864650231808'))
+    res.end()
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
